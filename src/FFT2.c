ccc96d53556a2e3ad392baca207790fdebbbacff
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2013-09-12 01:20:55 +0200

    Short-time const q transform

--- src/FFT2.c
+++ src/FFT2.c
@@ -1,22 +1,33 @@
+#include <stdlib.h>
 #include <math.h>
 #include <complex.h>
+#include <glib.h>
 
 #include <cblas.h>
 #include <fftw3.h>
 
-void dumpD(double* mem, int length) {
+void cq_dump_vector(double* mem, int length) {
     for (int j = 0; j < length; ++j) {
         printf("%.7lf\n", mem[j]);
     }
 }
 
-void dumpDC(double _Complex* mem, int length) {
+void cq_dump_vector_complex(double _Complex* mem, int length) {
     for (int j = 0; j < length; ++j) {
         printf("%.7lf + %.7lfi\n", creal(mem[j]), cimag(mem[j]));
     }
 }
 
-double* hanning(int length) {
+void cq_dump_matrix_abs(double _Complex* matrix, int height, int width) {
+    for (int j = 0; j < height; ++j) {
+        for (int k = 0; k < width; ++k)
+            printf("%.7lf ", cabs(matrix[j * width + k]));
+
+        printf("\n");
+    }
+}
+
+double* cq_hanning_window(int length) {
     double* result = fftw_alloc_real(length);
 
     for (int j = 0; j < length; ++j) {
@@ -27,7 +38,7 @@ double* hanning(int length) {
     return result;
 }
 
-fftw_complex* tempKernel(double Q, int length) {
+fftw_complex* cq_temp_kernel(double Q, int length) {
     fftw_complex* result = fftw_alloc_complex(length);
 
     for (int j = 0; j < length; ++j) {
@@ -38,7 +49,7 @@ fftw_complex* tempKernel(double Q, int length) {
     return result;
 }
 
-void elementMultiply(double* in, fftw_complex* in2, fftw_complex* out,
+void cq_multiply_vector_elem(double* in, fftw_complex* in2, fftw_complex* out,
         int length) {
     for (int j = 0; j < length; ++j) {
         fftw_complex elem = in[j] * in2[j];
@@ -46,19 +57,20 @@ void elementMultiply(double* in, fftw_complex* in2, fftw_complex* out,
     }
 }
 
-double q(int bins) {
+double cq_q(int bins) {
     return 1 / (exp2(1 / (double)bins) - 1);
 }
 
-int k(int minFreq, int maxFreq, int bins) {
-    return rint(ceil(bins * log2((double)maxFreq / (double)minFreq)));
+int cq_k(int min_freq, int max_freq, int bins) {
+    return rint(ceil(bins * log2((double)max_freq / (double)min_freq)));
 }
 
-int fftLen(double q_, int minFreq, int sampleRate) {
-    return rint(exp2(ceil(log2(q_ * sampleRate / (double) minFreq))));
+int cq_fft_len(double q, int min_freq, int sample_rate) {
+    return rint(exp2(ceil(log2(q * sample_rate / (double) min_freq))));
 }
 
-void zeroThresh(double _Complex* matrix, int length, double thresh) {
+void cq_zero_vector_below_thresh(double _Complex* matrix, int length,
+        double thresh) {
     fftw_complex zero = 0.0 + 0.0*_Complex_I;
     for (int j = 0; j < length; ++j) {
         if (cabs(matrix[j]) <= thresh)
@@ -66,19 +78,18 @@ void zeroThresh(double _Complex* matrix, int length, double thresh) {
     }
 }
 
-void swapColumn(double _Complex* matrix, int height, int width,
+void cq_swap_matrix_column(double _Complex* matrix, int height, int width,
         double _Complex* vector, int index) {
     for (int j = 0; j < height; ++j) {
         matrix[j * width + index] = vector[j];
     }
 }
 
-fftw_complex* kernel(int minFreq, int maxFreq, int sampleRate, int bins,
-        int* height, int* width) {
-
-    double Q = q(bins);
-    int K = k(minFreq, maxFreq, bins);
-    int fft = fftLen(Q, minFreq, sampleRate);
+fftw_complex* cq_make_kernel(int min_freq, int max_freq, int sample_rate,
+        int bins, int* height, int* width) {
+    double Q = cq_q(bins);
+    int K = cq_k(min_freq, max_freq, bins);
+    int fft = cq_fft_len(Q, min_freq, sample_rate);
 
     fftw_complex* temp = fftw_alloc_complex(fft);
     fftw_complex* spec = fftw_alloc_complex(fft);
@@ -87,19 +98,19 @@ fftw_complex* kernel(int minFreq, int maxFreq, int sampleRate, int bins,
             FFTW_ESTIMATE);
 
     for (int j = K-1; j >= 0; --j) {
-        int len = ceil(Q*sampleRate/(minFreq*exp2(j/(double)bins)));
+        int len = ceil(Q*sample_rate/(min_freq*exp2(j/(double)bins)));
 
-        double* h = hanning(len);
-        fftw_complex* vec = tempKernel(Q, len);
+        double* h = cq_hanning_window(len);
+        fftw_complex* vec = cq_temp_kernel(Q, len);
 
         // temp = h .* vec
-        elementMultiply(h, vec, temp, len);
+        cq_multiply_vector_elem(h, vec, temp, len);
 
         // spec = fft(temp)
         fftw_execute(plan);
 
-        zeroThresh(spec, fft, 0.0054);
-        swapColumn(result, fft, K, spec, j);
+        cq_zero_vector_below_thresh(spec, fft, 0.0054);
+        cq_swap_matrix_column(result, fft, K, spec, j);
 
         fftw_free(h);
         fftw_free(vec);
@@ -118,10 +129,11 @@ fftw_complex* kernel(int minFreq, int maxFreq, int sampleRate, int bins,
     return result;
 }
 
-fftw_complex* constQTransform(double* data, fftw_complex* kernel,
+fftw_complex* cq_const_q_transform(double* data, fftw_complex* kernel,
         int height, int width) {
     fftw_complex* fft = fftw_alloc_complex(height);
-    fftw_plan plan = fftw_plan_dft_r2c_1d(height, data, fft, FFTW_ESTIMATE);
+    fftw_plan plan = fftw_plan_dft_r2c_1d(height, data, fft, FFTW_ESTIMATE |
+            FFTW_PRESERVE_INPUT);
     fftw_execute(plan);
 
     // fill redundant data
@@ -147,21 +159,56 @@ fftw_complex* constQTransform(double* data, fftw_complex* kernel,
     return result;
 }
 
+fftw_complex* cq_short_time_constq_transform(double* data, int data_length,
+        int min_freq, int max_freq, int sample_rate, int bins, int step,
+        int* height,  int* width) {
+    int kernel_height, kernel_width;
+    fftw_complex* kern = cq_make_kernel(min_freq, max_freq, sample_rate, bins,
+        &kernel_height, &kernel_width);
+
+    int max_index = rint(ceil(data_length / kernel_height));
+
+    int indices_size = (max_index - 1) * kernel_height / (double) step + 1;
+    int* indices = (int*) malloc(indices_size * sizeof(int));
+    for (int j = 0, k = 0; j <= (max_index - 1) * kernel_height; ++k, j += step)
+        indices[k] = j;
+
+    fftw_complex* result = fftw_alloc_complex(kernel_width * indices_size);
+
+    for (int j = 0; j < indices_size; ++j) {
+        fftw_complex* cq = cq_const_q_transform(data + indices[j], kern,
+                kernel_height, kernel_width);
+
+        cq_swap_matrix_column(result, kernel_width, indices_size, cq, j);
+        fftw_free(cq);
+    }
+
+    fftw_free(kern);
+    free(indices);
+
+    *height = kernel_width;
+    *width = indices_size;
+    return result;
+}
+
 int main() {
-    FILE* f = fopen("output", "r");
-    double* data = fftw_alloc_real(16384);
+    FILE* f = fopen("ppp", "r");
+    double* data = fftw_alloc_real(524288);
     double num;
 
     int i = 0;
     while(fscanf(f, "%lf\n", &num) > 0)
         data[i++] = num;
 
-    int fft,K;
-    fftw_complex* vec = kernel(55, 880, 44100, 12, &fft, &K);
-    fftw_complex* cq = constQTransform(data, vec, fft, K);
-    dumpDC(cq, K);
+    int height, width;
+    GTimer* gt = g_timer_new();
+    fftw_complex* cq = cq_short_time_constq_transform(data, 524288,
+            55, 440, 44100, 12, 2048, &height, &width);
+    double elapsed = g_timer_elapsed(gt, NULL);
 
-    fftw_free(vec);
+    printf("%.8lf\n", elapsed);
+
+    g_timer_destroy(gt);
     fftw_free(cq);
     fftw_free(data);
     fclose(f);
